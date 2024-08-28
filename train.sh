13492601474250fe48549477363a0b356033754a
ZhuLvs <lushuaizhu722@gmail.com>
2024-08-28 13:20:48 +0800

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+# Set the path to the main.py file
+MAIN_PY_PATH="model/main.py"
+
+# Check if the main.py file exists
+if [ ! -f "$MAIN_PY_PATH" ]; then
+    echo "Error: $MAIN_PY_PATH not found."
+    exit 1
+fi
+
+# Run the Python script
+python "$MAIN_PY_PATH"
+
+echo "Script execution completed."
