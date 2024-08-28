f40bc5966d59a235a3236a1cb5e67f9d026e9169
ZhuLvs <lushuaizhu722@gmail.com>
2024-08-28 13:11:38 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-# Set the path to the main.py file
+
+# Set the paths to Length_main.py and main.py files
+LENGTH_MAIN_PY_PATH="model/Length_main.py"
 MAIN_PY_PATH="model/main.py"
 
-# Check if the main.py file exists
+# Check if Length_main.py file exists
+if [ ! -f "$LENGTH_MAIN_PY_PATH" ]; then
+    echo "Error: $LENGTH_MAIN_PY_PATH not found."
+    exit 1
+fi
+
+# Check if main.py file exists
 if [ ! -f "$MAIN_PY_PATH" ]; then
     echo "Error: $MAIN_PY_PATH not found."
     exit 1
 fi
 
-# Run the Python script
+# Run the Length_main.py script
+python "$LENGTH_MAIN_PY_PATH"
+
+# Run the main.py script
 python "$MAIN_PY_PATH"
+
 echo "Script execution completed."
