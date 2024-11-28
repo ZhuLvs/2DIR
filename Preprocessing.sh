6b5005feec04221029481b0ae365fab49e02eb4d
ZhuLvs <lushuaizhu722@gmail.com>
2024-11-28 11:28:38 +0800

    Update Preprocessing.sh

--- Preprocessing.sh
+++ Preprocessing.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
+
+
+
+if [ ! -d "data/valA" ]; then
+    echo "Creating data/valA folder..."
+    mkdir -p data/valA
+else
+    echo "data/valA already exists."
+fi
+
+if [ ! -d "data/valB" ]; then
+    echo "Creating data/valB folder..."
+    mkdir -p data/valB
+else
+    echo "data/valB already exists."
+fi
+
+echo "Running Calculate Distance Map.py..."
+python3 helper_scripts/Calculate\ Distance\ Map.py
+
+echo "Running Padding.py..."
+python3 helper_scripts/Padding.py
+
+echo "Running Dataset_split.py..."
+python3 helper_scripts/Dataset_split.py
+
+echo "Running move.py..."
+python3 helper_scripts/move.py
+
+echo "All tasks completed."
 
